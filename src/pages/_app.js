85ce3d81d2c4e5e429fce7dd7ae4e1beb444c5ea
wunlei <confusioncosmos@gmail.com>
2024-06-26 20:21:44 +0300

    Add project: metadata

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,28 @@
+import Head from "next/head";
+import { Roboto } from "next/font/google";
 import "@/styles/globals.scss";
 
+export const roboto = Roboto({
+  subsets: ["cyrillic"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>YourTour</title>
+      </Head>
+      <style jsx global>{`
+        body {
+          --font-roboto: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,33 @@ import { Html, Head, Main, NextScript } from "next/document";
 function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="YourTour – идеальный партнер для путешествий! Вы можете выбрать готовый тур или создать путешествие по своему вкусу. От автостопа по Стамбулу до треккинга в горах – сделайте ваш отдых незабываемым!"
+        />
+        <meta
+          name="keywords"
+          content="индивидуальные туры, планирование путешествия, туристические услуги, отдых на природе"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
